157ad45fe31867ea04447f48853c1f73228ea135
赵悦茗 <164242417+marco-zym@users.noreply.github.com>
2025-01-30 10:08:49 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-function zh(type) {
-    if (type == "online"){
-        window.open("https://marco-zym.github.io/guess-word/zh/online", "_self")
-    }
-    if (type == "offline"){
-        window.open("https://marco-zym.github.io/guess-word/zh/offline", "_self")
-    }
+function zh() {
+    window.open("https://marco-zym.github.io/guess-word/zh", "_self")
     
 }
 
-function en(type) {
-    if (type == "online"){
-        window.open("https://marco-zym.github.io/guess-word/en/online", "_self")
-    }
-    if (type == "offline"){
-        window.open("https://marco-zym.github.io/guess-word/en/offline", "_self")
-    }
+function en() {
+    window.open("https://marco-zym.github.io/guess-word/en/online", "_self")
 }
